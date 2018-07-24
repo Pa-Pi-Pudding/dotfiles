74743e2f74768ae3d593161bb5af90d877a28181
zaizchan <ramen.princess293so@gmail.com>
2018-07-24 19:15:31 +0900

    hoge

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -2,28 +2,30 @@
 
 case ${OSTYPE} in
     darwin*)
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew install zsh
-    brew install zplugA
-    git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-    ~/.bash_it/install.sh
+    sudo /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    sudo brew install zsh
+    sudo brew install zplugA
+    sudo git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+    sudo ~/.bash_it/install.sh
         ;;
     linux-gnu*)
     sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y autoclean
-    apt-get install git
-    spt-get install zsh
-    apt-get install curl
-    curl -sL --proto-redir -all,https https://zplug.sh/installer | zsh
-    git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-    ~/.bash_it/install.sh
-    source ~/.bashrc
+    sudo  apt install vim
+    sudo  apt install aptitude
+    sudo  aptitude install vim-gnome
+    sudo  spt install zsh
+    sudo  apt install curl
+    sudo  curl -sL --proto-redir -all,https https://zplug.sh/installer | zsh
+    sudo  git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+    sudo  ~/.bash_it/install.sh
+    sudo  source ~/.bashrc
     ;;
 esac
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-git clone https://github.com/powerline/fonts.git --depth=1
-pip3 install flake8
+sudo git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+sudo git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+sudo git clone https://github.com/powerline/fonts.git --depth=1
+sudo pip3 install flake8
 cd fonts
-./install.sh
+sudo ./install.sh
 cd ..
-rm -rf fonts
+sudo rm -rf fonts
